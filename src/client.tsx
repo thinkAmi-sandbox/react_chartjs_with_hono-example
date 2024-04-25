dde23faf7d9427e9b64e226203d0bbfd33e5e7f5
thinkAmi <dev.thinkami+git@gmail.com>
2024-04-25 21:56:18 +0900

    add chart

--- src/client.tsx
+++ src/client.tsx
@@ -1,9 +1,41 @@
 import * as React from "react"
 import {createRoot} from "react-dom/client"
+import {ArcElement, Chart as ChartJS, Legend, Tooltip} from 'chart.js'
+import {Pie} from 'react-chartjs-2'
+
+const ChartComponent = () => {
+  ChartJS.register(ArcElement, Tooltip, Legend)
+
+  const data = {
+    labels: ['奥州ロマン', 'シナノゴールド', 'ピンクレディ', 'ブラムリー'],
+    datasets: [
+      {
+        label: '購入数',
+        data: [1, 5, 3, 2],
+        backgroundColor: [
+          'firebrick', 'gold', 'pink', 'mediumseagreen'
+        ],
+        borderColor: [
+          'firebrick', 'gold', 'pink', 'mediumseagreen'
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
+
+  return (
+    <div style={{width: '300px'}}>
+      <Pie data={data} />
+    </div>
+  )
+}
 
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <ChartComponent/>
+    </>
   )
 }
 
