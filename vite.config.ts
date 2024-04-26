57af2bae13baf86865fc2e77da48a467c84c78a3
thinkAmi <dev.thinkami+git@gmail.com>
2024-04-26 09:08:12 +0900

    add HMR for WSL2

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,17 @@ export default defineConfig(({ mode }) => {
         devServer({
           entry: 'src/index.tsx'
         })
-      ]
+      ],
+      // refs: https://zenn.dev/hatobato/articles/1b22dbe585c9ae
+      // 以下のパラメータを指定しないとapp.css, app.jsのURLが0.0.0.0になってしまうため、明示的にlocalhostに変更
+      hmr: {
+        host: 'localhost'
+      },
+      // Windowsアプリでファイル編集した際に監視されない問題があるため、usePolling:trueにすることで監視を強制させる
+      // https://vitejs.dev/config/server-options.html#server-watch
+      watch: {
+        usePolling: true
+      }
     }
   }
 })
